fa3c867a0019e8d7c5e63952b111a13f545a05af
Shaun Warman <swarman@paypal.com>
2016-11-19 21:19:59 -0600

    Add channel tests

--- test/test-podium.js
+++ test/test-podium.js
@@ -3,14 +3,43 @@ const Podium = require('podium');
 const test = require('tape');
 
 
+/******
+
+ const podium = new Podium({
+
+   // event name
+   name: 'eventName',
+
+   // event channel(s) available
+   channels: ['channel1', 'channel2'] || false,
+
+   // cloned before passed, default false - passed as-is
+   clone: true || false,
+
+   // listener calls each object in array one by one
+   // with each individual argument
+   spread: true || false,
+
+   // appended to args list at the end (tag key, value true)
+   tags: ['tag1', 'tag2', 'tag3'] || false,
+
+   // same event name can be registered multiple times
+   shared: true || false
+
+ });
+
+
+ *****/
+
+
 test('test podium', (t) => {
 
     test('test new podium object', (t) => {
         const podium = new Podium();
 
-        t.ok(podium, 'Podium object created');
-        t.ok(typeof podium === 'object', 'Not an object!');
-        t.ok(podium instanceof Podium, 'Not an instance of Podium!');
+        t.ok(podium, 'Is initialized');
+        t.ok(typeof podium === 'object', 'Is an object');
+        t.ok(podium instanceof Podium, 'Instance of Podium');
 
         t.end();
     });
@@ -46,6 +75,42 @@ test('test podium', (t) => {
         });
 
         podium.emit('event2', [{ a: 1 }, { b: 2 }, { c: 3 }]);
+
+        t.end();
+    });
+
+    test('test podium channels', (t) => {
+
+        const podium1 = new Podium({
+            name: 'event1',
+            channels: ['channel1', 'channel2']
+        });
+
+        podium1.on({name: 'event1', channels: ['channel1']}, (data) => {
+            t.ok(data, 'Emitted data is defined');
+
+            const {a, b} = data;
+
+            t.equal(a, 1, 'Emitted value is the same');
+            t.equal(b, 2, 'Emitted value is the same');
+        });
+
+        podium1.on({name: 'event1', channels: ['channel2']}, (data) => {
+            t.ok(data, 'Emitted data is defined');
+
+            const {a, b} = data;
+
+            t.equal(a, 3, 'Emitted value is the same');
+            t.equal(b, 4, 'Emitted value is the same');
+        });
+
+        podium1.emit({name: 'event1', channel: 'channel1'}, {a:1, b:2}, () => {
+            console.log(`channel1 - everyone notified!`);
+        });
+
+        podium1.emit({name: 'event1', channel: 'channel2'}, {a:3, b:4}, () => {
+            console.log(`channel2 - everyone notified!`);
+        });
 
         t.end();
     });
